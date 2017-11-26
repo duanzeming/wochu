9a36781e8bb0d07f58d7073fe6aa0c2865c33461
wangbo <m18612399988@163.com>
2017-11-26 14:10:10 +0800

    首页

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -28,14 +28,141 @@
             <div class="new">
                 <div class="left-view">
                     <img src="https://wochu.oss-cn-hangzhou.aliyuncs.com/upload/64f0efe2-d8b3-475c-ba52-e6b6c70324a8.jpg" alt="">
+                    <div class="price">
+                        <p>0.01元抢3菜</p>
+                        <p class="ellipsis">新手专享</p>
+                    </div>
                 </div>
                 <div class="right-view">
-                    <div class="top"></div>
-                    <div class="bottom"></div>
+                    <div class="top">
+                        <img src="https://wochu.oss-cn-hangzhou.aliyuncs.com/upload/2076361b-8419-4c85-95ce-4f6c2d97d350.jpg" alt="">
+                        <div class="price">
+                            <p>29元包邮</p>
+                            <p class="ellipsis">光明莫斯利安</p>
+                        </div>
+                    </div>
+                    <div class="bottom">
+                        <img src="https://wochu.oss-cn-hangzhou.aliyuncs.com/upload/54503910-520e-4173-9178-db1d05bd09aa.jpg" alt="">
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="test clearfixed">wwwwww</div>
+        <!-- 我厨快报 -->
+        <div class="goods-item-list clearfixed">
+            <!-- logo -->
+            <div class="wckbLogo">
+                <img src="https://wochu.oss-cn-hangzhou.aliyuncs.com/upload/f58332d2-ad98-4d10-aa96-28d1b8090d37.jpg" alt="">
+            </div>
+            <!-- 快文播报 -->
+            <div class="wckbLogoRight">
+                <ul>
+                    <li>满88送减价值299的人马君减脂营课</li>
+                </ul>
+                <img src="../../assets/img/down@3x.png" alt="">
+            </div>
+        </div>
+        <!-- 超快加热餐 -->
+        <div class="ckjrc">
+            <div class="temp-title">
+                <span></span>
+                超快加热餐
+                <span class="line-rt"></span>
+            </div>
+            <ul>
+                <li>
+                    <div class="img-view">
+                        <img src="https://wochu.oss-cn-hangzhou.aliyuncs.com/upload/31d5e858-61eb-4019-8c2f-786725c2bacf.jpg" alt="">
+                    </div>
+                    <div class="temp-goods-info">
+                        <p class="temp-goods-name">
+                            <span></span>
+                            中粮家佳康精装冰鲜精肉丝240g
+                        </p>
+                        <div class="goods-intro">纯瘦肉，粗细均匀，自己切不出来的好肉丝</div>
+                        <div class="temp-goods-price">
+                            <div class="price-wrap">
+                                <div class="real-price">
+                                    ¥ <span class="before-point">15</span>
+                                    <span class="affter-point">.9</span>
+                                </div>
+                                <p class="markt-price">¥ 19.9</p>
+                            </div>
+                            <div class="add-to-cart">
+                                <img src="../../assets/img/add.png">
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <!-- 横向商品展示 -->
+        <div class="shop_show">
+            <ul>
+                <li>
+                    <img src="https://wochu.oss-cn-hangzhou.aliyuncs.com/upload/fe1d17ee-40e6-46f4-a3f0-188ae5bc30ab.jpg" alt="">
+                    <p>南非花皮脐橙12粒超值装</p>
+                    <span>￥29.9</span>
+                    <strong>￥14.9</strong>
+                    <div class="shop_show_img">
+                        <img src="../../assets/img/add.png" alt="">
+                    </div>
+                </li>
+                <li>
+                    <img src="https://wochu.oss-cn-hangzhou.aliyuncs.com/upload/fe1d17ee-40e6-46f4-a3f0-188ae5bc30ab.jpg" alt="">
+                    <p>南非花皮脐橙12粒超值装</p>
+                    <span>￥29.9</span>
+                    <strong>￥14.9</strong>
+                    <div class="shop_show_img">
+                        <img src="../../assets/img/add.png" alt="">
+                    </div>
+                </li>
+                <li>
+                    <img src="https://wochu.oss-cn-hangzhou.aliyuncs.com/upload/fe1d17ee-40e6-46f4-a3f0-188ae5bc30ab.jpg" alt="">
+                    <p>南非花皮脐橙12粒超值装</p>
+                    <span>￥29.9</span>
+                    <strong>￥14.9</strong>
+                    <div class="shop_show_img">
+                        <img src="../../assets/img/add.png" alt="">
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <!-- 更多团购 -->
+        <div class="goods-item clearfixed">
+            <div class="new">
+                <div class="left-view">
+                    <img src="https://wochu.oss-cn-hangzhou.aliyuncs.com/upload/16eda440-e42b-4a1e-964a-422411779355.jpg" alt="">
+                    <div class="price">
+                        <p>更多团购</p>
+                        <p class="ellipsis">黄鱼鲞双份包邮劲爆团</p>
+                    </div>
+                </div>
+                <div class="right-view">
+                    <div class="top">
+                        <img src="https://wochu.oss-cn-hangzhou.aliyuncs.com/upload/c00b2b3d-ca8f-4ec8-a4b5-3938964660f6.jpg" alt="">
+                        <div class="price">
+                            <p>自选套餐</p>
+                            <p class="ellipsis">特价套餐/任选好菜</p>
+                        </div>
+                    </div>
+                    <div class="bottom">
+                        <img src="https://wochu.oss-cn-hangzhou.aliyuncs.com/upload/3039fc75-8bad-497e-949a-9335bdd1033e.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- 团购下方 -->
+        <div class="more_shop_bottom">
+            <img src="https://wochu.oss-cn-hangzhou.aliyuncs.com/upload/a5b13de7-a1d6-45cf-8186-ce06edccc925.jpg" alt="">
+        </div>
+        <!-- 主页四小图 -->
+        <div class="home_photo">
+            <ul>
+                <li>
+                    <img src="" alt="">
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
     
@@ -126,14 +253,18 @@ export default {
         height:.85rem;
     }
     /* 限量抢购 */
+    .goods-item{
+        width: 100%;
+    }
     .new{
         padding:.15rem .1rem 0 .1rem;
         background:#f4f4f4;
     }
     .left-view{
         float:left;
-        width:1.75rem;
-        height:2.6rem;
+        width:50%;
+        /* height:2.6rem; */
+        position: relative;
     }
     .left-view img{
         width:100%;
@@ -141,6 +272,242 @@ export default {
     }
     .right-view{
         float:right;
+        width: 50%;
+        position: relative;
     }
-
+    .right-view img{
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+    }
+    .right-view .price,.left-view .price{
+        position: absolute;
+        top: .1rem;
+        left: .1rem;
+    }
+    .right-view .price p:nth-of-type(1),.left-view .price p:nth-of-type(1){
+        color: #001e00;
+        font-size: .14rem;
+        font-weight: 600;
+    }
+    .right-view .price p:nth-of-type(2),.left-view .price p:nth-of-type(2){
+        margin-top: .08rem;
+        color: #999;
+        font-size: .12rem;
+    }
+    /* 我厨快报 */
+    .goods-item-list{
+        width: 100%;
+        height: .58rem;
+        position: relative;
+    }
+    .wckbLogo{
+        float: left;
+        padding: .12rem 0 0 .12rem;
+    }
+    .wckbLogo img{
+        width: .42rem;
+        height: .3rem;
+        border-right: 1px solid #e5e5e5;
+        padding-right: .12rem;
+    }
+    .wckbLogoRight{
+        float: left;
+        /* width: 100%; */
+    }
+    .wckbLogoRight ul li{
+        width: 100%;
+        height: .57rem;
+        padding: .2rem .45rem .12rem .13rem;
+        color: #001e00;
+        font-size: .13rem;
+    }
+    .wckbLogoRight img{
+        width: .45rem;
+        position: absolute;
+        top: .24rem;
+        width: .15rem;
+        height: .08rem;
+        right: .12rem;
+    }
+    /* 超快加热餐 */
+    .ckjrc{
+        width: 100%;
+        height: 2.22rem;
+        padding-top: .15rem;
+        background: #f4f4f4;
+    }
+    .temp-title{
+        background: #fff;
+        height: auto;
+        min-height: .5rem;
+        text-align: center;
+        font-size: .16rem;
+        font-weight: 600;
+        padding: .15rem 0;
+        line-height: .2rem;
+        color: #001e00;
+        border-bottom: 1px solid #e5e5e5;
+    }
+    .temp-title span{
+        width: .75rem;
+        height: 1px;
+        display: inline-block;
+        border-bottom: 2px solid #011e00;
+        margin: .05rem .11rem;
+    }
+    .ckjrc ul{
+        background: #fff;
+    }
+    .ckjrc ul li{
+        width: 100%;
+        height: 1.56rem;
+        padding: .14rem .12rem;
+        border-bottom: 1px solid #e5e5e5;
+    }
+    .img-view{
+        float: left;
+        width: 1.3rem;
+        height: 1.3rem;
+    }
+    .img-view img{
+        width: 100%;
+        height: 100%;
+    }
+    .temp-goods-info{
+        float: right;
+        width: 2.17rem;
+        height: 100%;
+        padding-left: .1rem;
+    }
+    .temp-goods-name{
+        height: .5rem;
+        width: 100%;
+        line-height: .18rem;
+        font-size: .16rem;
+        font-weight: 700;
+        letter-spacing: .01rem;
+        padding-top: .16rem;
+        color: #666666;
+    }
+    .temp-goods-name span{
+        color: #6eb042;
+        font-size: .16rem;
+        height: .16rem;
+        font-weight: 700;
+        vertical-align: baseline;
+    }
+    .goods-intro{
+        width: 100%;
+        height: .28rem;
+        font-size: .13rem;
+        line-height: .28rem;
+        letter-spacing: .01rem;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        margin-bottom: .16rem;
+        color: #666666;
+    }
+    .price-wrap{
+        float: left;
+        line-height: .25rem;
+    }
+    .real-price{
+        float: left;
+        color: #ff5918;
+        font-size: .15rem;
+    }
+    .before-point{
+        font-size: .2rem;
+    }
+    .markt-price{
+        float: left;
+        font-size: .12rem;
+        text-decoration: line-through;
+        color: #999;
+        margin-left: .09rem;
+    }
+    .add-to-cart{
+        float: right;
+        width: .26rem;
+        height: .26rem;
+    }
+    .add-to-cart img{
+        width: 100%;
+        height: 100%;
+        vertical-align: text-top;
+    }
+    /* 横向商品展示 */
+    .shop_show{
+        width: 100%;
+        height: 1.89rem;
+        min-height: 1.63rem!important;
+        background: #fff;
+        padding: .13rem 0 0 .12rem;
+        font-size: 0;
+        z-index: 1;
+    }
+    .shop_show ul{
+        width: 100%;
+    }
+    .shop_show li{
+        float: left;
+        height: 1.76rem;
+        margin-right: .13rem;
+        width: .95rem;
+        vertical-align: top;
+        position: relative;
+    }
+    .shop_show li img{
+        width: .95rem;
+        height: .95rem;
+    }
+    .shop_show li p{
+        vertical-align: top;
+        font-size: .11rem;
+        color: #666666;
+        font-weight: bolder;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+    .shop_show li span{
+        display: block;
+        height: .12rem;
+        margin-top: .07rem;
+        font-size: .1rem;
+        color: #bababa;
+        text-decoration: line-through;
+        margin-top: .08rem;
+    }
+    .shop_show li strong{
+        height: .12rem;
+        margin-top: .05rem;
+        font-size: .12rem;
+        color: #001e00;
+        line-height: .12rem;
+        font-weight: normal;
+        display: block;
+        margin: .03rem;
+    }
+    .shop_show_img{
+        position: absolute;
+        bottom: .2rem;
+        right: .71em;
+        width: .19rem;
+        /* height: .19rem; */
+    }
+    .shop_show li .shop_show_img img{
+        width: 100%;
+        height: 100%;
+    }
+    /* 团购下方 */
+    .more_shop_bottom{
+        width: 100%;
+    }
+    .more_shop_bottom img{
+        width: 100%;
+        height: 100%;
+    }
 </style>
